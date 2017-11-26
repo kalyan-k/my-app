7e4b99faf14f4d201f8d05ac5bcd962b7a7eb5a9
temp <temp@dummy.com>
2017-11-26 23:02:59 +0530

    yet to work on exp list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ExpensesComponent } from './expenses/expenses.component';
+import { ExpensesListComponent } from './expenses-list/expenses-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    ExpensesComponent
+    ExpensesComponent,
+    ExpensesListComponent
   ],
   imports: [
     BrowserModule,

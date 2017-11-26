1ed1bef6418866529f4d0021229a98dde99e6c07
temp <temp@dummy.com>
2017-11-26 15:25:28 +0530

    Added bootstrap, routing and 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+    { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes, { enableTracing: true, useHash: true })
+    ],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
